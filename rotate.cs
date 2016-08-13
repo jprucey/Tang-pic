c450eaddc761c43a85e19a50507d8e1d5bc6f4c3
jprucey <jimprucey@yahoo.com>
2016-08-13 18:46:06 -0400

    Add files via upload

---
+++ rotate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+		this.transform.rotation = Random.rotation;
+
+	
+	}
+	
+	// Update is called once per frame
+	 void Update () {
+		
+		this.transform.Rotate(Vector3.right * Time.deltaTime* (Random.Range(10f, 20f)));
+
+
+	
+	}
+
+	public void OnMouseDown(){
+		// this object was clicked - do something
+				transform.localScale += new Vector3(1.0f,1.0f,1.0f)*Time.deltaTime;
+	}
+}
